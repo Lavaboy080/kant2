2b041847c53c4cd34785e40fe6795809420765b0
Lavaboy080 <sigurd.melby@outlook.com>
2025-10-14 10:54:41 +0200

    lagde tre orbøker og importerte datetime i app.py den første orbøken inneholder oversettinger den andre inneholder hvor mye varene koster den tredje orboken har matrettene sin beskrivelse

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template
+import datetime
+
+dager = {'Monday':'Mandag','Tuesday':'Tirsdag','Wednesday':'Ondsdag','Thursday':'Torsdag','Friday':'Fredag','Saturday':'Lørdag','Sunday':'Søndag'}
+verdi = {'Pølser':'15kr','Risgrøt':'10kr','Pizza':'35kr','Grønnsaksuppe':'20kr','Salat':'10kr','Skolefri':'ingen mat','Skolefri':'ingen mat'}
+beskribelse ={'Pølser':'Pølse i brød','Risgrøt':'Glutenfri Risgrøt','Pizza':'Pizzastykke med peperoni','Grønnsaksuppe':'Suppe med gulrot og potet','Salat':'Salat med tomat og agurk'}
+idag = datetime.datetime.now().strftime("%A")
 
 app = Flask(__name__)
 
