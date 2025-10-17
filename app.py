be448359dd4ab396e6f999e6df6f28408f44db8b
Lavaboy080 <sigurd.melby@outlook.com>
2025-10-17 15:05:39 +0200

    lagde en liste med verdier i app.py og lagde en dagens meny i meny.html

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import datetime
 
 app = Flask(__name__)
 
-dager = {'Monday':'Mandag','Tuesday':'Tirsdag','Wednesday':'Ondsdag','Thursday':'Torsdag','Friday':'Fredag'}
+matretter = ['Pølser','Risgrøt','Pizza','Grønnsaksuppe','Salat']
+pris = ['15kr','10kr','35kr','20kr','10kr']
 ukedager = ['Mandag','Tirsdag','Ondsdag','Torsdag','Fredag']
+dager = {'Monday':'Mandag','Tuesday':'Tirsdag','Wednesday':'Ondsdag','Thursday':'Torsdag','Friday':'Fredag'}
 verdi = {'Pølser':'15kr','Risgrøt':'10kr','Pizza':'35kr','Grønnsaksuppe':'20kr','Salat':'10kr'}
 beskribelse ={'Pølser':'Pølse i brød','Risgrøt':'Glutenfri Risgrøt','Pizza':'Pizzastykke med peperoni','Grønnsaksuppe':'Suppe med gulrot og potet','Salat':'Salat med tomat og agurk'}
 dag = datetime.datetime.now().strftime("%A")
@@ -17,7 +19,7 @@ def index():
 
 @app.route('/meny')
 def meny():
-    return render_template("meny.html",dager=dager,verdi=verdi,beskribelse=beskribelse,dag=dag,ukedager=ukedager)
+    return render_template("meny.html",dager=dager,verdi=verdi,beskribelse=beskribelse,dag=dag,ukedager=ukedager,matretter=matretter,pris=pris)
 
 @app.route('/varer')
 def varer():
