a32cef620e5878a69a1a411974851fd783022a57
Lavaboy080 <sigurd.melby@outlook.com>
2025-10-17 13:29:14 +0200

    lagde en navigasjonsbar og slettet tildige navigasjonsmetoder

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
 import datetime
 
+app = Flask(__name__)
+
 dager = {'Monday':'Mandag','Tuesday':'Tirsdag','Wednesday':'Ondsdag','Thursday':'Torsdag','Friday':'Fredag','Saturday':'Lørdag','Sunday':'Søndag'}
 verdi = {'Pølser':'15kr','Risgrøt':'10kr','Pizza':'35kr','Grønnsaksuppe':'20kr','Salat':'10kr','Skolefri':'ingen mat','Skolefri':'ingen mat'}
 beskribelse ={'Pølser':'Pølse i brød','Risgrøt':'Glutenfri Risgrøt','Pizza':'Pizzastykke med peperoni','Grønnsaksuppe':'Suppe med gulrot og potet','Salat':'Salat med tomat og agurk'}
 dag = datetime.datetime.now().strftime("%A")
 
-app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -26,5 +28,5 @@ def kontakt():
 
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
