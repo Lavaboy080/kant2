cad33ee7a70e879dd447cd1894cbe0679ea281f5
Lavaboy080 <sigurd.melby@outlook.com>
2025-10-25 19:45:00 +0200

    la til igredienser i varer og endret litt på bildene

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ import datetime
 app = Flask(__name__)
 
 matretter = ['Pølser','Risgrøt','Pizza','Grønnsaksuppe','Salat']
+beskribelse =['Pølse i brød med ketchup','Risgrøt med kanel','Pizzastykke med peperoni','Suppe med gulrot og potet','Salat med tomat og agurk']
+igredienser = ['Melk','Ris','Sukkker','Purre','Gulrot','Brekkbønner','Tomat','Agurk','Isbergsalat','Pølse','Pølsebrød','Ketchup','Mel','Gjær','Vann','Olje','Salt']
 pris = ['15kr','10kr','35kr','20kr','10kr']
 bilder2 = ['../static/bilder/Pølse.png','../static/bilder/Risgrøt.png','../static/bilder/Pizza.png','../static/bilder/Grønnsaksuppe.png','../static/bilder/Salat.png']
 bilder = {'Monday':'../static/bilder/Pølse.png','Tuesday':'../static/bilder/Risgrøt.png','Wednesday':'../static/bilder/Pizza.png','Thursday':'../static/bilder/Grønnsaksuppe.png','Friday':'../static/bilder/Salat.png'}
 ukedager = ['Mandag','Tirsdag','Onsdag','Torsdag','Fredag']
 dager = {'Monday':'Mandag','Tuesday':'Tirsdag','Wednesday':'Onsdag','Thursday':'Torsdag','Friday':'Fredag'}
 verdi = {'Pølser':'15kr','Risgrøt':'10kr','Pizza':'35kr','Grønnsaksuppe':'20kr','Salat':'10kr'}
-beskribelse ={'Pølser':'Pølse i brød','Risgrøt':'Glutenfri Risgrøt','Pizza':'Pizzastykke med peperoni','Grønnsaksuppe':'Suppe med gulrot og potet','Salat':'Salat med tomat og agurk'}
 dag = datetime.datetime.now().strftime("%A")
 
 
@@ -21,11 +22,11 @@ def index():
 
 @app.route('/meny')
 def meny():
-    return render_template("meny.html",dager=dager,verdi=verdi,beskribelse=beskribelse,dag=dag,ukedager=ukedager,matretter=matretter,pris=pris,bilder=bilder,bilder2 = bilder2)
+    return render_template("meny.html",dager=dager,verdi=verdi,beskribelse=beskribelse,dag=dag,ukedager=ukedager,matretter=matretter,pris=pris,bilder=bilder,bilder2 = bilder2,igredienser=igredienser)
 
 @app.route('/varer')
 def varer():
-    return render_template("varer.html",dager=dager,verdi=verdi,beskribelse=beskribelse,dag=dag,ukedager=ukedager,matretter=matretter,pris=pris,bilder=bilder,bilder2 = bilder2)
+    return render_template("varer.html",dager=dager,verdi=verdi,beskribelse=beskribelse,dag=dag,ukedager=ukedager,matretter=matretter,pris=pris,bilder=bilder,bilder2 = bilder2,igredienser=igredienser)
 
 @app.route('/kontakt')
 def kontakt():
