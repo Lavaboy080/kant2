49e8a527df9c46b72c3a9980976f8a7eb27cf357
Lavaboy080 <sigurd.melby@outlook.com>
2025-10-31 12:49:52 +0100

    la til litt ingredienser

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 
 matretter = ['Pølser','Risgrøt','Pizza','Grønnsaksuppe','Salat']
 beskribelse =['Pølse med tilbehør','Risgrøt med tilbehør','Pizzastykke med peperoni','Suppe med gulrot og potet','Salat med tomat og agurk']
-igredienser = ['Melk','Grøtris','Sukker','Purre','Gulrot','Brekkbønner','Tomat','Agurk','Isbergsalat','Pølse','Pølsebrød','Ketchup','Mel','Gjær','Vann','Olje','Sennep','Smør','Kanel','Pepperoni']
+igredienser = ['Melk','Grøtris','Sukker','Purre','Gulrot','Brekkbønner','Tomat','Agurk','Isbergsalat','Pølse','Pølsebrød','Ketchup','Mel','Gjær','Vann','Olje','Sennep','Smør','Kanel','Pepperoni','Brød','Ost','Reker','Mr le kopp','Skinke']
 pris = ['15kr','10kr','35kr','20kr','10kr']
 fastvarer = ['Vann','Melk','Rekesmørbrød','Nudler','Toast']
 fastpris = ['Gratis','5kr','15kr','10kr','15kr']
 ukedager = ['Mandag','Tirsdag','Onsdag','Torsdag','Fredag']
 bilder4 = ['../static/bilder/Vann.png','../static/bilder/Melk.png','../static/bilder/Rekesmørbrød.png','../static/bilder/Nudler.png','../static/bilder/Toast.png']
-bilder3 = ['../static/bilder/Melk.png','../static/bilder/Grøtris.png','../static/bilder/Sukker.png','../static/bilder/Purre.png','../static/bilder/Gulrot.png','../static/bilder/Brekkbønner.png','../static/bilder/Tomat.png','../static/bilder/Agurk.png','../static/bilder/Isbergsalat.png','../static/bilder/Pølsepakker.png','../static/bilder/Pølsebrød.png','../static/bilder/Ketchup.png','../static/bilder/Mel.png','../static/bilder/Gjær.png','../static/bilder/Vann.png','../static/bilder/Olje.png','../static/bilder/Sennep.png','../static/bilder/Smør.png','../static/bilder/Kanel.png','../static/bilder/Pepperoni.png']
+bilder3 = ['../static/bilder/Melk.png','../static/bilder/Grøtris.png','../static/bilder/Sukker.png','../static/bilder/Purre.png','../static/bilder/Gulrot.png','../static/bilder/Brekkbønner.png','../static/bilder/Tomat.png','../static/bilder/Agurk.png','../static/bilder/Isbergsalat.png','../static/bilder/Pølsepakker.png','../static/bilder/Pølsebrød.png','../static/bilder/Ketchup.png','../static/bilder/Mel.png','../static/bilder/Gjær.png','../static/bilder/Vann.png','../static/bilder/Olje.png','../static/bilder/Sennep.png','../static/bilder/Smør.png','../static/bilder/Kanel.png','../static/bilder/Pepperoni.png','../static/bilder/Brød.png','../static/bilder/Ost.png','../static/bilder/Reker.png','../static/bilder/Nudler.png','../static/bilder/skinke.png']
 bilder2 = ['../static/bilder/Pølse.png','../static/bilder/Risgrøt.png','../static/bilder/Pizza.png','../static/bilder/Grønnsaksuppe.png','../static/bilder/Salat.png']
 bilder = {'Monday':'../static/bilder/Pølse.png','Tuesday':'../static/bilder/Risgrøt.png','Wednesday':'../static/bilder/Pizza.png','Thursday':'../static/bilder/Grønnsaksuppe.png','Friday':'../static/bilder/Salat.png'}
 dager = {'Monday':'Mandag','Tuesday':'Tirsdag','Wednesday':'Onsdag','Thursday':'Torsdag','Friday':'Fredag'}
