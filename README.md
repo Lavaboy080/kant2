5e5ad57da9e6219d7655ae1815289e58c5d241fc
Lavaboy080 <sigurd.melby@outlook.com>
2025-10-31 09:11:49 +0100

    lagde ferdig readme og la til ingredienser

---
+++ README.md
@@ -0,0 +1,14 @@
+# KANTINE 
+
+## Inhold 
+
+Skal være en navigasjonsmeny som viser vei til kontaktinfo meny hjemsiden varer og ingredienser.
+Skal være info i hver eneste route.
+css, html og pythonspråket er brukt for dette.
+
+## Kjøring
+Før å kjøre programmet må du ha filene og finne dem i din terminal.
+Det er også viktig å aktivere env med source env/bin/activate.
+
+## Kilder
+Jeg har brukt chat gpt til å foreslå kode 
--- app.py
+++ app.py
@@ -7,10 +7,13 @@ matretter = ['Pølser','Risgrøt','Pizza','Grønnsaksuppe','Salat']
 beskribelse =['Pølse med tilbehør','Risgrøt med tilbehør','Pizzastykke med peperoni','Suppe med gulrot og potet','Salat med tomat og agurk']
 igredienser = ['Melk','Grøtris','Sukker','Purre','Gulrot','Brekkbønner','Tomat','Agurk','Isbergsalat','Pølse','Pølsebrød','Ketchup','Mel','Gjær','Vann','Olje','Sennep','Smør','Kanel','Pepperoni']
 pris = ['15kr','10kr','35kr','20kr','10kr']
+fastvarer = ['Vann','Melk','Rekesmørbrød','Nudler','Toast']
+fastpris = ['Gratis','5kr','15kr','10kr','15kr']
+ukedager = ['Mandag','Tirsdag','Onsdag','Torsdag','Fredag']
+bilder4 = ['../static/bilder/Vann.png','../static/bilder/Melk.png','../static/bilder/Rekesmørbrød.png','../static/bilder/Nudler.png','../static/bilder/Toast.png']
 bilder3 = ['../static/bilder/Melk.png','../static/bilder/Grøtris.png','../static/bilder/Sukker.png','../static/bilder/Purre.png','../static/bilder/Gulrot.png','../static/bilder/Brekkbønner.png','../static/bilder/Tomat.png','../static/bilder/Agurk.png','../static/bilder/Isbergsalat.png','../static/bilder/Pølsepakker.png','../static/bilder/Pølsebrød.png','../static/bilder/Ketchup.png','../static/bilder/Mel.png','../static/bilder/Gjær.png','../static/bilder/Vann.png','../static/bilder/Olje.png','../static/bilder/Sennep.png','../static/bilder/Smør.png','../static/bilder/Kanel.png','../static/bilder/Pepperoni.png']
 bilder2 = ['../static/bilder/Pølse.png','../static/bilder/Risgrøt.png','../static/bilder/Pizza.png','../static/bilder/Grønnsaksuppe.png','../static/bilder/Salat.png']
 bilder = {'Monday':'../static/bilder/Pølse.png','Tuesday':'../static/bilder/Risgrøt.png','Wednesday':'../static/bilder/Pizza.png','Thursday':'../static/bilder/Grønnsaksuppe.png','Friday':'../static/bilder/Salat.png'}
-ukedager = ['Mandag','Tirsdag','Onsdag','Torsdag','Fredag']
 dager = {'Monday':'Mandag','Tuesday':'Tirsdag','Wednesday':'Onsdag','Thursday':'Torsdag','Friday':'Fredag'}
 verdi = {'Pølser':'15kr','Risgrøt':'10kr','Pizza':'35kr','Grønnsaksuppe':'20kr','Salat':'10kr'}
 dag = datetime.datetime.now().strftime("%A")
@@ -23,11 +26,16 @@ def index():
 
 @app.route('/meny')
 def meny():
-    return render_template("meny.html",dager=dager,verdi=verdi,beskribelse=beskribelse,dag=dag,ukedager=ukedager,matretter=matretter,pris=pris,bilder=bilder,bilder2 = bilder2,igredienser=igredienser,bilder3=bilder3)
+    return render_template("meny.html",dager=dager,verdi=verdi,beskribelse=beskribelse,dag=dag,ukedager=ukedager,matretter=matretter,pris=pris,bilder=bilder,bilder2=bilder2,igredienser=igredienser,bilder3=bilder3,bilder4 =bilder4,fastpris=fastpris,fastvarer=fastvarer)
 
 @app.route('/varer')
 def varer():
-    return render_template("varer.html",dager=dager,verdi=verdi,beskribelse=beskribelse,dag=dag,ukedager=ukedager,matretter=matretter,pris=pris,bilder=bilder,bilder2 = bilder2,igredienser=igredienser,bilder3=bilder3)
+    return render_template("varer.html",dager=dager,verdi=verdi,beskribelse=beskribelse,dag=dag,ukedager=ukedager,matretter=matretter,pris=pris,bilder=bilder,bilder2=bilder2,igredienser=igredienser,bilder3=bilder3,bilder4 =bilder4,fastpris=fastpris,fastvarer=fastvarer)
+
+@app.route('/ingredienser')
+def ingridienser():
+    return render_template("ingredienser.html",dager=dager,verdi=verdi,beskribelse=beskribelse,dag=dag,ukedager=ukedager,matretter=matretter,pris=pris,bilder=bilder,bilder2=bilder2,igredienser=igredienser,bilder3=bilder3,bilder4 =bilder4,fastpris=fastpris,fastvarer=fastvarer)
+
 
 @app.route('/kontakt')
 def kontakt():
